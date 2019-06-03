9a0865ac44479c7fb3475d768652b4995cb0dcb5
shmily-zhao <51266907+shmily-zhao@users.noreply.github.com>
2019-06-03 15:31:35 +0800

    Add files via upload

---
+++ 161403322赵彩霞/code/BPlusTree/BPlusTree.h
@@ -0,0 +1,42 @@
+//
+//  BPlusTree.h
+//  BPlusTree
+//
+//  Created by Wuyixin on 2017/8/4.
+//  Copyright © 2017年 Coding365. All rights reserved.
+//
+ 
+#ifndef BPlusTree_h
+#define BPlusTree_h
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+ 
+#define M (4)
+#define LIMIT_M_2 (M % 2 ? (M + 1)/2 : M/2)
+ 
+typedef struct BPlusNode *BPlusTree,*Position;
+typedef int KeyType;
+struct BPlusNode{
+    int KeyNum;
+    KeyType Key[M + 1];
+    BPlusTree Children[M + 1];
+    BPlusTree Next;
+};
+ 
+/* 初始化 */
+extern BPlusTree Initialize();
+/* 插入 */
+extern BPlusTree Insert(BPlusTree T,KeyType Key);
+/* 删除 */
+extern BPlusTree Remove(BPlusTree T,KeyType Key);
+/* 销毁 */
+extern BPlusTree Destroy(BPlusTree T);
+/* 遍历节点 */
+extern void Travel(BPlusTree T);
+/* 遍历树叶节点的数据 */
+extern void TravelData(BPlusTree T);
+ 
+#endif /* BPlusTree_h */
+
